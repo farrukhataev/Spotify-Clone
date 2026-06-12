d95a3a310826958e4edcb3b9aac6c8cb687f4d62
farrukhataev <farrukhataevl@gmail.com>
2026-06-12 08:52:04 +0300

    Creating Album Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 import MainLayout from "./layout/MainLayout.tsx";
 import ChatPage from "./pages/chat/ChatPage.tsx";
+import AlbumPage from "./pages/album/AlbumPage.tsx";
 
 export default function App() {
    //token =>
@@ -18,7 +19,7 @@ export default function App() {
        <Route element= {<MainLayout/>}>
             <Route path='/' element={<HomePage />} />
             <Route path='/chat' element={<ChatPage />} />
-
+            <Route path='/albums/:albumId' element={<AlbumPage />} />
        </Route>
      </Routes>
    </>
--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -6,12 +6,14 @@ export interface Song {
   imageUrl:string;
   audioUrl:string;
   duration:number;
-  createdAt:Date;
-  updatedAt:Date;
+  createdAt:string;
+  updatedAt:string;
 }
 export interface Album {
   _id:string;
   title:string;
   artist:string;
   imageUrl:string;
+  releaseYear:number;
+  songs:Song[];
 }
